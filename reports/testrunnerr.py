1d0b2bd71024d68de41a6c4f7940e6cd98a4611b
“gangya” <18739570240@163.com>
2024-09-10 04:01:11 +0800

    [1]测试执行代码封装为函数

--- reports/testrunnerr.py
+++ reports/testrunnerr.py
@@ -1,7 +1,8 @@
 import unittest
 from BSTestRunner import BSTestRunner
 
-cases = unittest.defaultTestLoader.discover("../testcases", "test*.py")
-file_name = open("report.html", "wb")
-runner = BSTestRunner(stream=file_name, title="测试报告", description="测试报告描述")
-runner.run(cases)
+def runtest():
+    cases = unittest.defaultTestLoader.discover("../testcases", "test*.py")
+    file_name = open("report.html", "wb")
+    runner = BSTestRunner(stream=file_name, title="测试报告", description="测试报告描述")
+    runner.run(cases)
